cfcb7350bfacc0c588f67609ac8e2c53db35e263
hongy-Li <511329028@qq.com>
2017-03-04 00:55:15 +0800

    1.不知道写什么

--- src/main/java/com/app/library/net/BaseHttp.java
+++ src/main/java/com/app/library/net/BaseHttp.java
@@ -1,6 +1,10 @@
 package com.app.library.net;
 
 import com.app.library.dialog.BaseDialog;
+import com.lhy.netlibrary.BaseNet;
+import com.lhy.netlibrary.IRequestListener;
+import com.lhy.netlibrary.NetFactory;
+import com.lhy.netlibrary.okhttp.OKHttpUtils;
 
 import org.json.JSONObject;
 
@@ -10,50 +14,42 @@ import java.util.Map;
  * Created by lhy on 2017/2/7
  */
 public abstract class BaseHttp<T extends IHttpListener> {
-    public static final String TAG = "BaseHttp";
-    protected static final int REQUEST_METHOD_POST = 0;
-    protected static final int POST = 1;
-    protected static final int REQUEST_METHOD_GET = 2;
-    protected static final String HTTP_CODE_ERROR = "-9999";
-    protected static final String HTTP_CODE_SUCCESS = "SUCCESS";
-    private static final String HTTP_CODE_KICK_OFF = "kick_off";//被别的账号挤掉
-    private static final String HTTP_CODE_SESSION_FAILURE = "session_failure";//登录过期
-    private static final String HTTP_CODE_SYS_ERROR = "SYS_ERROR";//登录过期
-    private static final String HTTP_CODE_SYSTEM_ERROR = "SYSTEM_ERROR";//登录过期
-    private String mClassName;
-
+    protected static final int HTTP_GET = 1000;
     public T mListener;
 
     public BaseHttp(T listener) {
         mListener = listener;
     }
 
-    protected void sendRequest(final BaseDialog dialog, String url, Map<String, Object> params, int method) {
-        mClassName = this.getClass().getSimpleName();
-
-    }
-
-    private void parseFailData(int code, String msg) {
-        switch (code) {
-
-            default:
-                parseFailData(HTTP_CODE_ERROR, msg);
+    protected void sendRequest(boolean isDialogShow, final BaseDialog dialog, String url, Map<String, Object> params, int method) {
+        if (isDialogShow && dialog != null) {
+            dialog.show();
+        }
+        OKHttpUtils net = NetFactory.createNetUtils(NetFactory.NetType.OK_HTTP);
+        switch (method) {
+            case HTTP_GET:
+                get(net, url, params);
                 break;
         }
-    }
-
-    private void parseSuccessData(String result) {
 
     }
 
-    public abstract void sendRequest(BaseDialog dialog, Object... params);
+    private void get(BaseNet net, String url, Map<String, Object> params) {
+        net.getHttp(url, params, new IRequestListener() {
+            @Override
+            public void onSucceed(String msg) {
 
-    /**
-     * @param ret        HTTP_CODE_SUCCESS 成功，HTTP_CODE_NO_DATA 没有数据
-     * @param jsonObject 解析后json
-     */
-    protected abstract void parseData(String ret, JSONObject jsonObject);
+            }
 
-    protected abstract void parseFailData(String ret, String jsonObject);
+            @Override
+            public void onFailed(Exception e) {
 
+            }
+        });
+    }
+
+    protected void parseSucceed(String string) {
+    }
+
+    ;
 }
